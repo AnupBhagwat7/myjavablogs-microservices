52e377ece0a2c703b6e876dbb59054c7c232d7f0
Anup Bhagwat <anup.bhagwat7@gmail.com>
2023-05-21 18:05:32 +0530

    Micrometer dependency and configuration

--- customer/src/main/java/in/myjavablog/CustomerConfig.java
+++ customer/src/main/java/in/myjavablog/CustomerConfig.java
@@ -1,5 +1,8 @@
 package in.myjavablog;
 
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,5 +15,10 @@ public class CustomerConfig {
     @LoadBalanced
     public RestTemplate template(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public Capability capability(final MeterRegistry registry) {
+        return new MicrometerCapability(registry);
     }
 }
