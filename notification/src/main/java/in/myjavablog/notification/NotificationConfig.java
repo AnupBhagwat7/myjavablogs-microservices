faf0addd71832e4d60cb7c1040e7e712fb07397d
Anup Bhagwat <anup.bhagwat7@gmail.com>
2023-05-21 21:29:25 +0530

    Micrometer configuration

---
+++ notification/src/main/java/in/myjavablog/notification/NotificationConfig.java
@@ -0,0 +1,16 @@
+package in.myjavablog.notification;
+
+import feign.Capability;
+import feign.micrometer.MicrometerCapability;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class NotificationConfig {
+
+    @Bean
+    public Capability capability(final MeterRegistry registry) {
+        return new MicrometerCapability(registry);
+    }
+}
